fc58de1a722354f89fd038f0d30249b16f1282e7
Agustin Brutti <bruttiagustin@gmail.com>
2022-12-05 22:52:31 -0300

    se agrega el proyecto

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Zapatillas Jordan IV",
+    precio: 13000,
+    img:
+      "https://storage.googleapis.com/thehundreds/media/2015/09/round-two-melrose_the-hundreds-julian-berman_08.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Campera Cold Nave",
+    precio: 7000,
+    img:
+      "https://storage.googleapis.com/thehundreds/media/2015/09/round-two-melrose_the-hundreds-julian-berman_11.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Buzo Sharks Nike",
+    precio: 8500,
+    img:
+      "https://storage.googleapis.com/thehundreds/media/2015/09/round-two-melrose_the-hundreds-julian-berman_12.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Remera Tommy",
+    precio: 4500,
+    img:
+      "https://storage.googleapis.com/thehundreds/media/2015/09/round-two-melrose_the-hundreds-julian-berman_14.jpg",
+    cantidad: 1,
+  },
+];
